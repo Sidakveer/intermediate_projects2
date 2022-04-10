2d5ca585c61cd1d1d4646aa448282fd3599ee596
Sidakveer <sidakveersingh8@gmail.com>
2022-04-10 13:00:29 -0400

    add code to start building the ping pong game

---
+++ ping_pong_game/main.py
@@ -0,0 +1,81 @@
+from turtle import Turtle, Screen
+import time
+
+
+class Paddle(Turtle):
+
+    def __init__(self, pos: tuple):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(pos)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("White")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
+
+
+
+screen = Screen()
+
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+
+ball = Ball()
+
+screen.listen()
+screen.onkey(right_paddle.go_up,"Up")
+screen.onkey(right_paddle.go_down,"Down")
+screen.onkey(left_paddle.go_up,"w")
+screen.onkey(left_paddle.go_down,"s")
+
+
+
+
+
+
+
+game_on = True
+while game_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+
+
+
+screen.mainloop()
