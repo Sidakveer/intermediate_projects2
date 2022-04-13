519c2f8a7b68dad8295f98dd2dcb7b3c071dc54f
Sidakveer <sidakveersingh8@gmail.com>
2022-04-13 11:55:22 -0400

    create scoreboard class

--- ping_pong_game/main.py
+++ ping_pong_game/main.py
@@ -40,6 +40,24 @@ class Ball(Turtle):
         self.y_move *= -1
 
 
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("White")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.score_update()
+
+    def score_update(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
 
 screen = Screen()
 
