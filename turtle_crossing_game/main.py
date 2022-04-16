fa8ce2f80cd8762a11f0042bd2a58baa29b89d1c
Sidakveer <sidakveersingh8@gmail.com>
2022-04-16 19:21:55 -0400

    make the cars move once every 6 times the loop runs

--- turtle_crossing_game/main.py
+++ turtle_crossing_game/main.py
@@ -17,13 +17,15 @@ screen.listen()
 screen.onkey(player1.move, "Up")
 
 game_is_on = True
+loop_count = 0
 while game_is_on:
+    loop_count += 1
     time.sleep(0.1)
     screen.update()
 
-    cars.create_car()
     cars.move1()
-
+    if loop_count % 6 == 0 or loop_count == 1:
+        cars.create_car()
 
 
 
