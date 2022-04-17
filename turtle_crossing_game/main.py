651d05aeed75f20e13da34e1a029d9116e9ec461
Sidakveer <sidakveersingh8@gmail.com>
2022-04-17 19:43:06 -0400

    update main.py

--- turtle_crossing_game/main.py
+++ turtle_crossing_game/main.py
@@ -12,20 +12,25 @@ screen.tracer(0)
 
 player1 = Player()
 cars = CarManager()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(player1.move, "Up")
 
 game_is_on = True
 loop_count = 0
+run_time = 0.1
 while game_is_on:
     loop_count += 1
-    time.sleep(0.1)
+    time.sleep(run_time)
     screen.update()
+
+    if player1.ycor() >= 290:
+        player1.finish()
+        score.level_up()
+        run_time *= 0.9
 
     cars.move1()
     if loop_count % 6 == 0 or loop_count == 1:
         cars.create_car()
-
-
 
