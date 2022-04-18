8ce074039977ca96ddebc824493398e326f26f9d
Sidakveer <sidakveersingh8@gmail.com>
2022-04-17 20:12:16 -0400

    main.py final code

--- turtle_crossing_game/main.py
+++ turtle_crossing_game/main.py
@@ -19,18 +19,29 @@ screen.onkey(player1.move, "Up")
 
 game_is_on = True
 loop_count = 0
-run_time = 0.1
+# run_time = 0.1
 while game_is_on:
     loop_count += 1
-    time.sleep(run_time)
+    time.sleep(0.1)
     screen.update()
 
-    if player1.ycor() > 280:
+    # level_up if player reaches the endline
+    if player1.ycor() >= 290:
         player1.finish()
         score.level_up()
-        run_time *= 0.9
+        cars.increase_speed()
+        # run_time *= 0.9
 
+    #create and move cars
     cars.move1()
     if loop_count % 6 == 0 or loop_count == 1:
         cars.create_car()
 
+    # detect collision with the car
+    for x in cars.all_cars:
+        if player1.distance(x) < 25:
+            score.game_over()
+            game_is_on = False
+
+
+screen.mainloop()
