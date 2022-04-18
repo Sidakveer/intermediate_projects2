bd539c7ba80e9e67a9ff8dee51a5e8a990f41fc8
Sidakveer <sidakveersingh8@gmail.com>
2022-04-17 20:12:42 -0400

    player.py final code

--- turtle_crossing_game/player.py
+++ turtle_crossing_game/player.py
@@ -2,7 +2,6 @@ from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
@@ -17,7 +16,8 @@ class Player(Turtle):
         self.showturtle()
 
     def move(self):
-        self.forward(10)
+        self.forward(MOVE_DISTANCE)
 
     def finish(self):
         self.goto(STARTING_POSITION)
+
